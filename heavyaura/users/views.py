6b0ff4d6d70635079f19c14a4cc588247cf96419
igrek121 <igrek121@gmail.com>
2025-05-12 23:33:06 +0500

    created orders

--- heavyaura/users/views.py
+++ heavyaura/users/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
-
+from django.contrib import auth
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from .forms import UserLoginForm
 
 def login(request):
-    return render(request, 'users/login.html')
+    if request.methot == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username,
+                                     password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('main:priduct'))
+        else:
+            form = UserLoginForm()
+
+    return render(request, 'users/login.html', {'form': form})
 
 
 def regisration(request):
